c40d74c84d7b1c5ea4472995e8a585bd826de172
Optider <anujsharmasppu@gmail.com>
2020-05-13 20:09:00 +0530

    Add merge() logic, thus making solution complete.

--- Divide and Conquer/2. Inversion Count.py
+++ Divide and Conquer/2. Inversion Count.py
@@ -1,7 +1,31 @@
 
 def merge(arr, start, end) :
     cross_invns = 0
-    # write merge
+
+    mid = (start + end) // 2
+    i = start
+    j = mid + 1
+    sorted_arr = []
+
+    while i <= mid and j <= end :
+        if arr[i] <= arr[j] :
+            sorted_arr.append(arr[i])
+            i += 1
+        else :
+            cross_invns += mid-i+1
+            sorted_arr.append(arr[j])
+            j += 1
+
+    # When any array exhausts, add the rem. elems of other array
+    if i <= mid :
+        sorted_arr.extend(arr[i:mid+1])
+    elif j<= end :
+        sorted_arr.extend(arr[j:end+1])
+    
+    # Update arr with sorted values
+    for i in range(len(sorted_arr)) :
+        arr[start+i] = sorted_arr[i]
+    
     return cross_invns
 
 def inversion_count(arr, start, end) :
@@ -15,7 +39,7 @@ def inversion_count(arr, start, end) :
     x = inversion_count(arr, start, mid)
     y = inversion_count(arr, mid+1, end)
     z = merge(arr, start, end)
-
+    # print(x,y,z)
     return x+y+z
 
 
