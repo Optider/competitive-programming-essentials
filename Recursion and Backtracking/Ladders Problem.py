21b47d6ad14ea36b77136374c6237a63a31d515e
Optider <anujsharmasppu@gmail.com>
2020-05-14 20:51:23 +0530

    Add logic for computing possible ways, ladder()

---
+++ Recursion and Backtracking/Ladders Problem.py
@@ -0,0 +1,19 @@
+def ladder(n, k) :
+    if n == 0 :
+        return 1
+    elif n < 0 :
+        return 0
+    ans = 0
+
+    # return ladder(n-1) + ladder(n-2) +...+ ladder(n-k)
+    for i in range(1, k+1) :
+        ans += ladder(n-i, k)
+
+    return ans
+    
+
+if __name__ == '__main__' :
+    n = 4
+    k = 3
+    no_of_sols = ladder(n, k)
+    print(no_of_sols)
