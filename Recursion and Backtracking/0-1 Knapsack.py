bf7ecc0e6f74ad220199f41b6a72805b96169e07
Optider <anujsharmasppu@gmail.com>
2020-05-17 13:11:03 +0530

    Add problem statement

---
+++ Recursion and Backtracking/0-1 Knapsack.py
@@ -0,0 +1,3 @@
+# Problem - A thief has a bag of max capacity 'C'. He wants to stole items so as to gain max profit.
+# Given : wt[], prices[] of 'N' items. 'C' is capacity of bag
+# Output : Max profit
