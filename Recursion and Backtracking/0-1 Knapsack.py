856fcc8ba24858c97952c8ec58248e43aafb1b8c
Optider <anujsharmasppu@gmail.com>
2020-05-17 13:11:52 +0530

    Add main()

--- Recursion and Backtracking/0-1 Knapsack.py
+++ Recursion and Backtracking/0-1 Knapsack.py
@@ -1,3 +1,12 @@
 # Problem - A thief has a bag of max capacity 'C'. He wants to stole items so as to gain max profit.
 # Given : wt[], prices[] of 'N' items. 'C' is capacity of bag
 # Output : Max profit
+
+if __name__ == '__main__' :
+    wt = [1, 2, 3, 5]
+    prices = [40, 20, 30, 100]
+
+    N = 4 # no of items
+    C = 7 # capacity of thief's bag
+
+    print(profit(N, C, wt, prices))
