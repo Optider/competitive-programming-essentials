904a7390bd6ef448e4495ba10773c5067abf7789
Optider <anujsharmasppu@gmail.com>
2020-05-18 22:08:26 +0530

    Add knapsack() logic, thus making solution complete

--- Recursion and Backtracking/0-1 Knapsack.py
+++ Recursion and Backtracking/0-1 Knapsack.py
@@ -1,6 +1,27 @@
 # Problem - A thief has a bag of max capacity 'C'. He wants to stole items so as to gain max profit.
 # Given : wt[], prices[] of 'N' items. 'C' is capacity of bag
 # Output : Max profit
+# Logic : if wt[i] > C exclude it. Else if wt[i] <= C then max(incluse, exclude)
+
+def knapsack(N, C, wt, prices) :
+
+    # Base Case
+    if N <= 0 or C <= 0 :
+        return 0
+
+    # Rec Case
+    max_profit = 0
+    include, exclude = 0, 0
+    
+    if wt[N-1] <= C :
+        # include curr item(last)
+        include = prices[N-1] + knapsack(N-1, C-wt[N-1], wt, prices)
+    
+    # exclude curr item(last)
+    exclude = knapsack(N-1, C, wt, prices)
+    
+    max_profit = max(include, exclude)
+    return max_profit
 
 if __name__ == '__main__' :
     wt = [1, 2, 3, 5]
@@ -9,4 +30,4 @@ if __name__ == '__main__' :
     N = 4 # no of items
     C = 7 # capacity of thief's bag
 
-    print(profit(N, C, wt, prices))
+    print(knapsack(N, C, wt, prices))
