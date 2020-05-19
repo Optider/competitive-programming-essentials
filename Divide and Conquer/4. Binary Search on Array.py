860f74e4fa53777e266a338c4adc5ac6db158d33
Optider <anujsharmasppu@gmail.com>
2020-05-19 23:47:06 +0530

    Add binar_search(), thus completing binary_search

--- Divide and Conquer/4. Binary Search on Array.py
+++ Divide and Conquer/4. Binary Search on Array.py
@@ -1,4 +1,19 @@
+def binary_search(arr, n) :
+    start = 0
+    end = n-1
+
+    while start <= end:
+        mid = (start + end)//2
+        
+        if key == arr[mid] :
+            return mid
+        elif arr[mid] < key :
+            start = mid + 1
+        elif arr[mid] > key :
+            end = mid - 1
+    return None
+
 if __name__ == "__main__" :
     arr = [1,2,3,4,5,6,10,20]
     key = 4
-    binary_search(arr, len(arr))
+    print(binary_search(arr, len(arr)))
