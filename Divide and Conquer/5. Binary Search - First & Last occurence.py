75d3ce1fd7f23df0a0fd18abed1d057d8cdde874
Optider <anujsharmasppu@gmail.com>
2020-05-20 11:54:05 +0530

    Add logic to find first occurence

--- Divide and Conquer/5. Binary Search - First & Last occurence.py
+++ Divide and Conquer/5. Binary Search - First & Last occurence.py
@@ -2,9 +2,31 @@
 # Given : Array, key
 # Output : Index of first & last occurence
 
-if __name__ == "__main__" :
-    arr = [1,2,3,4,6,6,6,6,12,18]
-    key = 6
+def first(arr, key) :
+
+    start = 0
+    end = len(arr)-1
+    ans = None # Assumed not present
+
+    while start <= end :
+        mid = (start + end) // 2
+
+        if arr[mid] == key :
+            ans = mid
+            
+            # since 1st occur is needed, move towards left
+            end = mid - 1
+        elif arr[mid] < key :
+            start = mid + 1
+        elif arr[mid] > key :
+            end = mid - 1
     
+    return ans
+
+if __name__ == "__main__" :
+    
+    arr = [1,2,4,6,6,6,6,12,18]
+    key = 6
+
     print(first(arr, key))
-    print(last(arr, key))
+    # print(last(arr, key))
