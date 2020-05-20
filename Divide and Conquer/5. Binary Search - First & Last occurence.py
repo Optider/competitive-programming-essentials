41cb63afcbd85805f3459a996159ba4a0f670872
Optider <anujsharmasppu@gmail.com>
2020-05-20 11:29:12 +0530

    Add main()

--- Divide and Conquer/5. Binary Search - First & Last occurence.py
+++ Divide and Conquer/5. Binary Search - First & Last occurence.py
@@ -1,3 +1,10 @@
 # Problem : Find first & last occurence of a no in the array
 # Given : Array, key
 # Output : Index of first & last occurence
+
+if __name__ == "__main__" :
+    arr = [1,2,3,4,6,6,6,6,12,18]
+    key = 6
+    
+    print(first(arr, key))
+    print(last(arr, key))
