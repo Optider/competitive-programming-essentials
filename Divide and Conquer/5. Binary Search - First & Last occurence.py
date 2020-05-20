a260c9f9184b3b1e21ddae7a0584957e624c13ea
Optider <anujsharmasppu@gmail.com>
2020-05-20 12:40:44 +0530

    Add logic to find last occurence

--- Divide and Conquer/5. Binary Search - First & Last occurence.py
+++ Divide and Conquer/5. Binary Search - First & Last occurence.py
@@ -23,10 +23,29 @@ def first(arr, key) :
     
     return ans
 
+def last(arr, key) :
+    start = 0
+    end = len(arr) - 1
+    ans = None
+
+    while start <= end :
+        mid = (start + end) // 2
+        if arr[mid] == key :
+            ans = mid
+
+            # since last occur is needed, move towards right
+            start = mid + 1
+        elif arr[mid] < key :
+            start = mid + 1
+        elif arr[mid] > key :
+            end = mid - 1
+    
+    return ans
+
 if __name__ == "__main__" :
     
     arr = [1,2,4,6,6,6,6,12,18]
     key = 6
 
     print(first(arr, key))
-    # print(last(arr, key))
+    print(last(arr, key))
