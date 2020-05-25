d3cc531dbb96165dffeac18a5f055bbde253830a
Optider <anujsharmasppu@gmail.com>
2020-05-25 18:27:26 +0530

    Add logic for finding integer root

--- Divide and Conquer/8. Binary Search in Monotonic Search Spaces(Square root).py
+++ Divide and Conquer/8. Binary Search in Monotonic Search Spaces(Square root).py
@@ -2,6 +2,29 @@
 # Input : N
 # Output : sqrt(N)
 
+def find_root(N) :
+
+    start = 1
+    end = N
+    ans = -1
+    while start <= end :
+        mid = (start+end) // 2
+
+        if mid*mid == N :
+            ans = mid
+            return ans
+        elif mid*mid < N :
+            ans = mid
+            start = mid + 1
+        elif mid*mid > N :
+            end = mid - 1
+
+    return ans
+
 if __name__ == "__main__" :
     N = 9
-    find_root(N)
+    print(find_root(N))
+    print(find_root(10))
+    print(find_root(0))
+    print(find_root(1))
+    print(find_root(2))
