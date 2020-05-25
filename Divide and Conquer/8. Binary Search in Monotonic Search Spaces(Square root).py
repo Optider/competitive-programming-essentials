d19126c5bd5a1538784e845a220ea9955d91d432
Optider <anujsharmasppu@gmail.com>
2020-05-25 23:39:32 +0530

    Add comment, thus completing the solution

--- Divide and Conquer/8. Binary Search in Monotonic Search Spaces(Square root).py
+++ Divide and Conquer/8. Binary Search in Monotonic Search Spaces(Square root).py
@@ -19,8 +19,15 @@ def find_root(N) :
         elif mid*mid > N :
             end = mid - 1
 
+    # return ans
+    # Till above(uncomment) for square root in integer
+    # -----------Square root in decimals-------------
     if ans == -1 :
         return ans
+
+    
+    # Decimal part using Brute Force
+
     deci = 0.1
     for _ in range(3) :
         ans += deci
@@ -28,10 +35,10 @@ def find_root(N) :
         while ans*ans < N :
             # print((_, deci, ans), end = " ")
             ans += deci
+        # new ans^2 overshoots N, so substract deci
         ans -= deci
         deci = deci/10
         
-    
     return ans
 
 if __name__ == "__main__" :
