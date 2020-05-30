68dda01285684cd803ff8cc3f4b4d90d333b1015
Optider <anujsharmasppu@gmail.com>
2020-05-30 11:07:46 +0530

    Add base code of BST

---
+++ Binary Search Tree/04. Sum of BST.py
@@ -0,0 +1,51 @@
+class BST_node :
+	def __init__(self, val) :
+		self.val = val
+		self.left = None
+		self.right = None
+
+
+class BST :
+	def __init__(self) :
+		self.root = None
+
+	def insert(self, val) :
+		if self.root == None :
+			self.root = BST_node(val)
+			return
+		
+		cur = self.root
+		new_node = BST_node(val)
+		while True :
+
+
+			if val < cur.val :
+				if cur.left != None :
+					cur = cur.left
+				else :
+					cur.left = new_node
+					return
+			else :
+				if cur.right != None :
+					cur = cur.right
+				else :
+					cur.right = new_node
+					return
+
+	def preorder(self, root) :
+		if root == None :
+			return
+		self.preorder(root.left)
+		print(root.val)
+		self.preorder(root.right)
+
+if __name__ == '__main__':
+	bst = BST()
+	# T = int(input())
+	while True :
+		# T -= 1
+		val = int(input())
+		if val == -1 :
+			break
+		bst.insert(val)
+	bst.preorder(bst.root)
