d62144b46433b28de56b2de4814bc74b3415b082
Optider <anujsharmasppu@gmail.com>
2020-06-03 11:40:51 +0530

    Add algorithm desc with concept link

---
+++ String Algorithms/01. Naive Algorithm.py
@@ -0,0 +1,13 @@
+# Problem : Prints all occurrences of pat[] in txt[]
+
+    # Input:  txt[] = "THIS IS A TEST TEXT"
+    #         pat[] = "TEST"
+    # Output: Pattern found at index 10
+
+    # Input:  txt[] =  "AABAACAADAABAABA"
+    #         pat[] =  "AABA"
+    # Output: Pattern found at index 0
+    #         Pattern found at index 9
+    #         Pattern found at index 12
+
+# Concept : https://www.geeksforgeeks.org/naive-algorithm-for-pattern-searching/
