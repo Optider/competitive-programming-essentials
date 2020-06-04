c7973cf96e7a2ffb010c3b83159768d87117d683
Optider <anujsharmasppu@gmail.com>
2020-06-04 21:30:10 +0530

    Add problem description with links

---
+++ Recursion and Backtracking/Permutations.py
@@ -0,0 +1,25 @@
+# Type : CB Recursion & Backtracking
+# Problem : Prints all permutations of given string (in alphabetical order)
+
+    # Input:  abc
+    # Output: 
+    #         abc
+    #         acb
+    #         bac
+    #         bca
+    #         cab
+    #         cba
+            
+    # Input:  eat
+    # Output: 
+    #         aet
+    #         ate
+    #         eat
+    #         eta
+    #         tae
+    #         tea
+            
+# Concept : 
+    # https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+    # https://www.geeksforgeeks.org/print-first-n-distinct-permutations-of-string-using-itertools-in-python/?ref=rp
+    # PYTHON ITERTOOLS (above link)
