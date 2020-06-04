fd055fc07fd03ff6dcb4f6a7125953c63fc1524a
Optider <anujsharmasppu@gmail.com>
2020-06-04 20:37:14 +0530

    Add logic to find the pattern

--- String Algorithms/01. Naive Algorithm.py
+++ String Algorithms/01. Naive Algorithm.py
@@ -11,3 +11,20 @@
     #         Pattern found at index 12
 
 # Concept : https://www.geeksforgeeks.org/naive-algorithm-for-pattern-searching/
+
+def naive_search(txt, pat):
+    
+    for i in range(0, len(txt)-len(pat)+1) :
+        
+        
+        for j in range(0, len(pat)) :
+            if txt[i+j] != pat[j] :
+                break
+        else :
+            # Pattern found
+            print('Pattern found at index ', i)
+
+        
+        pass
+
+naive_search("AABAACAADAABAABA", "AABA")
