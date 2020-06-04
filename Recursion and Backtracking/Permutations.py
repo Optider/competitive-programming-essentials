a23771cdd9c1bda30ba23bd0931ba077a07df758
Optider <anujsharmasppu@gmail.com>
2020-06-04 23:53:53 +0530

    Add permutation logic

--- Recursion and Backtracking/Permutations.py
+++ Recursion and Backtracking/Permutations.py
@@ -23,3 +23,25 @@
     # https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
     # https://www.geeksforgeeks.org/print-first-n-distinct-permutations-of-string-using-itertools-in-python/?ref=rp
     # PYTHON ITERTOOLS (above link)
+
+
+def permute_all(word, i) :
+
+    # Base Case
+    if i >= len(word) :
+        print(''.join(word))
+        return
+
+    # Recursive Case
+
+    for j in range(i, len(word)) :
+        # swap : transfer word[i] to fixed region
+        word[i], word[j] = word[j], word[i]
+        permute_all(word, i+1)
+
+        # Backtrack
+        word[i], word[j] = word[j], word[i]
+
+
+    pass
+permute_all(list("abc"), 0)
