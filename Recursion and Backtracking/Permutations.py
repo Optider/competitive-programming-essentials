91213a8eb2019c21cab4b7d94f4170db371ddbc1
Optider <anujsharmasppu@gmail.com>
2020-06-05 19:16:57 +0530

    Add permutation logic using built-in module

--- Recursion and Backtracking/Permutations.py
+++ Recursion and Backtracking/Permutations.py
@@ -23,7 +23,14 @@
     # https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
     # https://www.geeksforgeeks.org/print-first-n-distinct-permutations-of-string-using-itertools-in-python/?ref=rp
     # PYTHON ITERTOOLS (above link)
+    
+    
+    # CHECK THIS CODE for same logic but lexicographically
+    # https://aakash01.github.io/programming/2014/03/11/permutations-of-a-given-string-in-lexicographic-order
+    # 2. Use iter tools
+    
 
+from itertools import permutations 
 
 def permute_all(word, i) :
 
@@ -44,4 +51,19 @@ def permute_all(word, i) :
 
 
     pass
+
+def using_builtin(word) :
+    '''
+        permutes lexicographically !!!
+        from itertools import permutations
+    '''
+
+    perm_list = permutations(word)
+    print('\nLexicographically using built-in module')
+    for i in perm_list :
+        print(''.join(i))
+
+
 permute_all(list("abc"), 0)
+
+using_builtin('abc')
