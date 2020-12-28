8dad6d2d4be2a4dd109dcb156c28c47ff9a060a8
Anuj Sharma <anujsharmasppu@gmail.com>
2020-12-28 15:17:48 +0530

    add theory-problem

--- 03.string/A.04.leftmost-repeating-char.cpp
+++ 03.string/A.04.leftmost-repeating-char.cpp
@@ -13,10 +13,59 @@ using namespace std;
 const int LEN{256};  // ascii char are 256
 // if only lowercase are there. Use 26 instead!
 
-// T: Theta(N) 2 loops
+// T: Theta(N+LEN) 1 loop
+// S: Theta(LEN) constant space
+// space is saved as bool[] is used as compared to int[] in better_1
+int leftmost_rep_better_2(string word) {
+    bool visited[LEN];
+    fill(visited, visited + LEN, false);
+
+    int leftmost_char_idx{INT_MAX};
+
+    // traverse word from RHS
+    for (int i = word.length() - 1; i >= 0; i--) {
+        char letter{word[i]};
+        // if letter is not visited, mark visited
+        if (visited[letter] == false)
+            visited[letter] = true;
+        // else leftmost_char_idx=current_idx, as it appeared again
+        else
+            leftmost_char_idx = i;
+    }
+
+    if (leftmost_char_idx == INT_MAX)
+        return -1;
+    return leftmost_char_idx;
+}
+
+// T: Theta(N+LEN) 1 loop
+// S: Theta(LEN) constant space
+int leftmost_rep_better_1(string word) {
+    int first_idx[LEN]{};  // -1 => unvisited, any-num-else => 1st index of visited char
+
+    fill(first_idx, first_idx + LEN, -1);
+
+    int leftmost_char_idx{INT_MAX};
+
+    for (int i{}; i < word.length(); i++) {
+        char letter = word[i];
+        if (first_idx[letter] == -1) {
+            first_idx[letter] = i;
+        } else {
+            // repeating char
+            leftmost_char_idx = min(leftmost_char_idx, first_idx[letter]);
+        }
+    }
+    if (leftmost_char_idx == INT_MAX) {
+        return -1;
+    }
+    return leftmost_char_idx;
+}
+
+// T: Theta(N+LEN) 2 loops
 // S: Theta(LEN) constant space
 int leftmost_rep(string word) {
-    int freq[LEN]{0};
+    int freq[LEN]{};
     for (char c : word) {
         freq[c]++;
     }
@@ -34,7 +83,9 @@ int leftmost_rep(string word) {
 int main() {
     string word{};
     cin >> word;
-    cout << leftmost_rep(word);
+    cout << leftmost_rep_better_2(word) << endl;
+    cout << leftmost_rep_better_1(word) << endl;
+    cout << leftmost_rep(word) << endl;
     cout << endl;
     return 0;
 }
