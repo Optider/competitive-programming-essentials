b865178fd65cf8bc3a5ece7d1dc50cc9116b153a
Anuj Sharma <anujsharmasppu@gmail.com>
2020-12-29 13:51:23 +0530

    add theory-problem sol-2

--- 03.string/A.05.leftmost-nonrepeat-char.cpp
+++ 03.string/A.05.leftmost-nonrepeat-char.cpp
@@ -17,7 +17,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int first_nonrep(string word) {
+const int LEN{256};
+
+// T: Theta(N) 2 loops for string
+// S: Theta(LEN)
+int first_nonrep_better_1(const string &word) {
+    int freq[LEN]{0};
+
+    for (char c : word)
+        freq[c]++;
+
+    for (int i{}; i < word.length(); i++) {
+        if (freq[word[i]] == 1)
+            return i;
+    }
+    return -1;
+}
+
+// T: Theta(N^2)
+// S: Theta(1)
+int first_nonrep(const string &word) {
     bool is_nonrep;
 
     for (int i{}; i < word.length(); i++) {
@@ -40,6 +59,8 @@ int main() {
     string word;
     cin >> word;
 
-    cout << first_nonrep(word) << endl;
+    // cout << first_nonrep(word) << endl;
+    cout << first_nonrep_better_1(word) << endl;
+
     return 0;
 }
