c2d7ac6d8ac07a1f8b971be22f06e66f43448cbf
Anuj Sharma <anujsharmasppu@gmail.com>
2021-01-09 12:24:09 +0530

    add theory-problem

--- 03.string/A.08.anagram-search.cpp
+++ 03.string/A.08.anagram-search.cpp
@@ -16,7 +16,60 @@ using namespace std;
 
 #define alpha_len 256
 
-// T: O(m+alpha_len)
+// T: O(n)
+// S: Theta(1) constant, alpha_len
+void anagram_search_better(const string &txt, const string &pat);
+bool is_count_empty(const int *count);
+bool is_anagram(const string &txt, const string &pat, int start);
+// T: O((n-m+1)*m)
+// S: Theta(1) => alpha_len, due to is_anagram(), constant
+void anagram_search(const string &txt, const string &pat);
+
+// T: O(1) alpha_len, constant
+// S: Theta(1) constant
+bool is_count_empty(const int *count) {
+    for (int i = 0; i < alpha_len; i++)
+        if (count[i] != 0)
+            return false;
+    return true;
+}
+
+// T: O(n)
+// S: Theta(1) constant, alpha_len
+void anagram_search_better(const string &txt, const string &pat) {
+    int n = txt.length();
+    int m = pat.length();
+
+    int count[alpha_len]{};
+
+    for (int i = 0; i < m; i++) {
+        count[pat[i]]--;
+        count[txt[i]]++;
+    }
+
+    // check if count[] is 0
+    if (is_count_empty(count)) {
+        cout << "Yes" << endl;
+        return;
+    }
+
+    for (int i = m; i < n; i++) {
+        // exclude txt[i-m] in count
+        count[txt[i - m]]--;
+        // include txt[i] in count
+        count[txt[i]]++;
+
+        // check if count[] is 0
+        if (is_count_empty(count)) {
+            cout << "Yes" << endl;
+            return;
+        }
+    }
+
+    cout << "No" << endl;
+}
+
+// T: O(m) <=> O(m+alpha_len)
 // S: Theta(1) constant, alpha_len
 bool is_anagram(const string &txt, const string &pat, int start) {
     int m = pat.length();
@@ -28,16 +81,12 @@ bool is_anagram(const string &txt, const string &pat, int start) {
         count[pat[j]]--;
     }
 
-    for (int f : count) {
-        if (f != 0)
-            return false;
-    }
-    return true;
+    return is_count_empty(count);
 }
 
 // T: O((n-m+1)*m)
 // S: Theta(1) => alpha_len, due to is_anagram(), constant
-void anagram_search(string txt, string pat) {
+void anagram_search(const string &txt, const string &pat) {
     int n = txt.length();
     int m = pat.length();
 
@@ -59,7 +108,9 @@ int main() {
     string txt, pat;
     cin >> txt >> pat;
 
+    anagram_search_better(txt, pat);
     anagram_search(txt, pat);
+
     cout << endl;
     return 0;
 }
