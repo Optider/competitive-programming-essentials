8ebc3579b99b8b277eecd1c4d143f7384c23da30
Anuj Sharma <anujsharmasppu@gmail.com>
2021-02-03 21:17:08 +0530

    add solution

--- 05.sorting/A.01.sort-stl.cpp
+++ 05.sorting/A.01.sort-stl.cpp
@@ -1,8 +1,13 @@
 /*  
+    Mainly used for sorting data stored in containers that allow random access (like array, vect, deque).
     Using sort() in cpp STL
-    1. arrays and vector
-    2. descending order
-    3. custom sort
+        1. arrays and vector
+        2. descending order
+        3. custom sort
+
+    NOTE:
+        Avg and worst case: O(nlogn)
+        Uses quick sort(by default), when quick is not optimum intro sort (quick, heap, insert)
 */
 
 #include <bits/stdc++.h>
@@ -26,19 +31,12 @@ bool customCmp(Point a, Point b) {
 }
 
 int main() {
-    // Arrays sort
+    // 1. Arrays sort
 
     int arr[] = {2, 6, 1, 7, 7, 3};
     int n = sizeof(arr) / sizeof(arr[0]);
     sort(arr, arr + n);
 
-    for (int a : arr)
-        cout << a << " ";
-    cout << endl;
-
-    // Descending order
-
-    sort(arr, arr + n, greater<int>());
     for (int a : arr)
         cout << a << " ";
     cout << endl;
@@ -52,7 +50,14 @@ int main() {
         cout << a << " ";
     cout << endl;
 
-    // Custom sort
+    // 2. Descending order
+
+    sort(arr, arr + n, greater<int>());
+    for (int a : arr)
+        cout << a << " ";
+    cout << endl;
+
+    // 3. Custom sort
     Point pointsArr[] = {{3, 10}, {1, 8}, {2, 8}, {3, 8}, {5, 4}};
     n = sizeof(pointsArr) / sizeof(pointsArr[0]);
     sort(pointsArr, pointsArr + n, customCmp);
