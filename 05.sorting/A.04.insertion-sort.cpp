2e56ba8e63d85646cc8527ec3e9027763513fd30
Anuj Sharma <anujsharmasppu@gmail.com>
2021-02-04 00:06:36 +0530

    add algo

---
+++ 05.sorting/A.04.insertion-sort.cpp
@@ -0,0 +1,36 @@
+/*  
+    Insertion Sort
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// T:
+// S:
+void insertionSort(vector<int> &arr, int n) {
+    int sortedIdx = {};
+    int cur = {};
+    for (int i = 1; i < n; i++) {
+        sortedIdx = i - 1;
+        cur = arr[i];
+
+        while (sortedIdx >= 0 and arr[sortedIdx] > cur) {
+            arr[sortedIdx + 1] = arr[sortedIdx];
+            sortedIdx--;
+        }
+        arr[sortedIdx + 1] = cur;
+    }
+}
+
+int main() {
+    vector<int> arr = {2, 6, 4, 1, 10, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+
+    for (int i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
