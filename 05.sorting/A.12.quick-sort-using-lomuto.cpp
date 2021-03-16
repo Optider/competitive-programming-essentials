39899aabfce2a92daeda4898211520715df6e7c2
Anuj Sharma <anujsharmasppu@gmail.com>
2021-03-16 18:50:21 +0530

    add sol template for lomuto partition

---
+++ 05.sorting/A.12.quick-sort-using-lomuto.cpp
@@ -0,0 +1,15 @@
+/*  
+    QuickSort using the method of Lomuto Partition.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// T:
+// S:
+// void solve() {}
+
+int main() {
+    cout << endl;
+    return 0;
+}
